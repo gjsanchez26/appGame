870b97ec308173eb55f5da04659198980488795d
gjsanchez26 <gjsanchez26@gmail.com>
2017-11-13 16:44:59 -0600

    third

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  films: Observable<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,  public http: Http) {
 
+  }
+  pressB():void{
+    console.log("peliiiigroooooo");
+    this.films = this.http.get('http://google.com');
+    console.log(this.films);
+    this.films
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    })
+    
+  
   }
 
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -3,7 +3,6 @@
  * more info on how to use sw-toolbox to custom configure your service worker.
  */
 
-
 'use strict';
 importScripts('./build/sw-toolbox.js');
 
@@ -18,6 +17,7 @@ self.toolbox.precache(
     './build/vendor.js',
     './build/main.css',
     './build/polyfills.js',
+    './pages/about/about.js'
     'index.html',
     'manifest.json'
   ]
@@ -29,3 +29,5 @@ self.toolbox.router.any('/*', self.toolbox.fastest);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
+
+
